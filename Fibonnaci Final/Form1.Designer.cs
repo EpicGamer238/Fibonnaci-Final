cbecbb4f09e60ae7732db38026a71794796ff756
Simon Wunderlich <wun0003@nhs.vic.edu.au>
2023-03-29 19:32:46 +1100

    ALMOST: add difference between index 1 and 2

--- Fibonnaci Final/Form1.Designer.cs
+++ Fibonnaci Final/Form1.Designer.cs
@@ -28,8 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             numericUpDown1 = new NumericUpDown();
+            panel1 = new Panel();
+            label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // numericUpDown1
@@ -38,25 +42,55 @@
             numericUpDown1.Name = "numericUpDown1";
             numericUpDown1.Size = new Size(42, 23);
             numericUpDown1.TabIndex = 0;
-            numericUpDown1.Value = new decimal(new int[] { 3, 0, 0, 0 });
+            numericUpDown1.Value = new decimal(new int[] { 1, 0, 0, 0 });
             numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // panel1
+            // 
+            panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
+            panel1.BackgroundImageLayout = ImageLayout.Stretch;
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(333, 316);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(141, 122);
+            panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 28F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ControlText;
+            label1.Location = new Point(25, 23);
+            label1.MaximumSize = new Size(90, 50);
+            label1.MinimumSize = new Size(90, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(90, 50);
+            label1.TabIndex = 0;
+            label1.Text = "Test";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Click += label1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
             Controls.Add(numericUpDown1);
             Name = "Form1";
             Text = "Form1";
-            Load += Form1_Load;
             Paint += Form1_Paint;
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private NumericUpDown numericUpDown1;
+        private Panel panel1;
+        private Label label1;
     }
 }
--- Fibonnaci Final/Form1.cs
+++ Fibonnaci Final/Form1.cs
@@ -1,21 +1,19 @@
+using System.Windows.Forms;
+
 namespace Fibonnaci_Final
 {
     public partial class Form1 : Form
     {
         Graphics g;
+        int x = 1;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
         public ulong fib(int index)
         {
-            index--;
             List<ulong> fib = new List<ulong>();
             fib.Add(1);
             fib.Add(1);
@@ -28,22 +26,38 @@ namespace Fibonnaci_Final
 
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
-            drawRect(g);
+
+
         }
 
-        private void drawRect(Graphics e)
+        private void drawRect()
         {
-            int count = Convert.ToInt32(fib(Convert.ToInt32(numericUpDown1.Value)));
+
+            var rand = new Random();
             int formHeight = this.Height;
             int formWidth = this.Width;
-            Pen blackPen = new Pen(Color.FromArgb(255, 0, 0, 0), 5);
-            e.DrawRectangle(blackPen, 0, 0, formWidth / count, formHeight);
+            Pen blackPen = new Pen(Color.FromArgb(255, 0, 0, 0), 1);
+            int count = Convert.ToInt32(fib(Convert.ToInt32(x) - 1));
+            for (int i = 0; i < count; i++)
+            {
+                SolidBrush randBrush = new SolidBrush(Color.FromArgb(255, rand.Next(256), rand.Next(256), rand.Next(256)));
+                Rectangle rect = new Rectangle(0 + (i * formWidth / count), 0, formWidth / count, formHeight / Convert.ToInt32(Math.Pow(2,x)));
+                this.CreateGraphics().FillRectangle(randBrush, rect);
+                this.CreateGraphics().DrawRectangle(blackPen, rect);
+            }
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             g = e.Graphics;
-            drawRect(g);
+            drawRect();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            x++;
+            label1.Text = x.ToString();
+            drawRect();
         }
     }
 }
